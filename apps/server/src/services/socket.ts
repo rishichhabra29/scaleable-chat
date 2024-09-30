5e492737a4ef187cef2644e6d74e4ff700613df5
Rishi Chhabra <106726718+rishichhabra29@users.noreply.github.com>
2024-09-30 17:03:15 +0530

    Update socket.ts

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,58 +1,64 @@
 import {Server} from "socket.io"
 import Redis from 'ioredis'
+import prismaClient from "./prisma";
 
 const pub=new Redis({
-    host: '',
+    host: 'caching-3b1b9c5f-chhabrarishi2001-0b95.h.aivencloud.com',
     port: 22604,
     username: 'default',
-    password: '',
+    password: 'AVNS_iTynHSSCDFFyfqOTQsr',
 });
 const sub=new Redis({
-    host: '',
+    host: 'caching-3b1b9c5f-chhabrarishi2001-0b95.h.aivencloud.com',
     port: 22604,
     username: 'default',
-    password: '',
+    password: 'AVNS_iTynHSSCDFFyfqOTQsr',
 
 });
 
 
 
 class SocketService {
-  private _io: Server;
-    constructor(){
-        console.log("INIT Socket Service...");
-        this._io=new Server({
-            cors:{
-                allowedHeaders: ["*"],
-                origin: "*",
-
-            },
-        });
-        sub.subscribe('MESSAGES')
-    }
-    public initListeners() {
-        const io=this.io;
-        console.log('Init Sockets Listeners....');
-        io.on("connect",(socket)=>{
-            console.log(`New Socket Connected `,socket.id);
-            socket.on("event:message",async({message}:{message:string})=>{
-                console.log("New message Rec.",message);
-                await pub.publish("MESSAGES",JSON.stringify({message}));
-
-
-            });
-
-        });
-        sub.on('message',(channel,message)=>{
-            if(channel==='MESSAGES'){
-                console.log('new message from redis',message)
-                io.emit("message",message);
-            }
-        });
-        
-    }
-    get io() {
-        return this._io;
-    }
-}
-export default SocketService;
+    private _io: Server;
+      constructor(){
+          console.log("INIT Socket Service...");
+          this._io=new Server({
+              cors:{
+                  allowedHeaders: ["*"],
+                  origin: "*",
+  
+              },
+          });
+          sub.subscribe('MESSAGES')
+      }
+      public initListeners() {
+          const io=this.io;
+          console.log('Init Sockets Listeners....');
+          io.on("connect",(socket)=>{
+              console.log(`New Socket Connected `,socket.id);
+              socket.on("event:message",async({message}:{message:string})=>{
+                  console.log("New message Rec.",message);
+                  await pub.publish("MESSAGES",JSON.stringify({message}));
+  
+  
+              });
+  
+          });
+          sub.on('message',async (channel,message)=>{
+              if(channel==='MESSAGES'){
+                  console.log('new message from redis',message)
+                  io.emit("message",message);
+                  await prismaClient.message.create({
+                    data:{
+                        text: message,
+                    }
+                  })
+              }
+          });
+          
+      }
+      get io() {
+          return this._io;
+      }
+  }
+  export default SocketService;
